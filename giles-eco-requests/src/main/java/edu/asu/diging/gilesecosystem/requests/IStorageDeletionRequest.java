a1db57ce3da0a2e5a1521bff198aa6ed139b6757
diya17 <diyanna.3@gmail.com>
2023-02-28 21:16:57 -0700

    [GECO-139] : Adding properties to StorageDeletionRequest

--- giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/IStorageDeletionRequest.java
+++ giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/IStorageDeletionRequest.java
@@ -1,5 +1,11 @@
 package edu.asu.diging.gilesecosystem.requests;
 
 public interface IStorageDeletionRequest extends IRequest {
+    public abstract void setUsername(String username);
+
+    public abstract String getUsername();
     
+    public abstract void setFileType(FileType fileType);
+
+    public abstract FileType getFileType();
 }
--- giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/impl/StorageDeletionRequest.java
+++ giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/impl/StorageDeletionRequest.java
@@ -1,12 +1,41 @@
 package edu.asu.diging.gilesecosystem.requests.impl;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import edu.asu.diging.gilesecosystem.requests.FileType;
 import edu.asu.diging.gilesecosystem.requests.IStorageDeletionRequest;
 
 public class StorageDeletionRequest extends Request implements IStorageDeletionRequest  {
     public final static String REQUEST_TYPE = "giles.request_type.storage_deletion";
-
+    
+    @JsonProperty
+    private FileType fileType;
+    
+    @JsonProperty
+    private String username;
+    
     @Override
     public String getType() {
         return REQUEST_TYPE;
+    }
+    
+    @Override
+    public FileType getFileType() {
+        return fileType;
+    }
+    
+    @Override
+    public void setFileType(FileType fileType) {
+        this.fileType = fileType;
+    }
+    
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
