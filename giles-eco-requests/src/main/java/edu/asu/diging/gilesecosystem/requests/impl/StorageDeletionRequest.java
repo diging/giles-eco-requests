b8f542b98de06c592c90e9cf07f19184df319fc1
diya17 <diyanna.3@gmail.com>
2023-05-23 16:59:19 -0700

    [GECO-139] Making changes to request

--- giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/impl/StorageDeletionRequest.java
+++ giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/impl/StorageDeletionRequest.java
@@ -8,34 +8,34 @@ import edu.asu.diging.gilesecosystem.requests.IStorageDeletionRequest;
 public class StorageDeletionRequest extends Request implements IStorageDeletionRequest  {
     public final static String REQUEST_TYPE = "giles.request_type.storage_deletion";
     
-    @JsonProperty
-    private String storageFileId;
-    
-    @JsonProperty
-    private boolean isOldFileVersion;
-    
+//    @JsonProperty
+//    private String storageFileId;
+//    
+//    @JsonProperty
+//    private boolean isOldFileVersion;
+//    
     @Override
     public String getType() {
         return REQUEST_TYPE;
     }
-    
-    @Override
-    public String getStorageFileId() {
-        return storageFileId;
-    }
-
-    @Override
-    public void setStorageFileId(String storageFileId) {
-        this.storageFileId = storageFileId;
-    }
-
-    @Override
-    public boolean getIsOldFileVersion() {
-        return this.isOldFileVersion;
-    }
-
-    @Override
-    public void setIsOldFileVersion(boolean isOldFileVersion) {
-        this.isOldFileVersion = isOldFileVersion;
-    }
+//    
+//    @Override
+//    public String getStorageFileId() {
+//        return storageFileId;
+//    }
+//
+//    @Override
+//    public void setStorageFileId(String storageFileId) {
+//        this.storageFileId = storageFileId;
+//    }
+//
+//    @Override
+//    public boolean getIsOldFileVersion() {
+//        return this.isOldFileVersion;
+//    }
+//
+//    @Override
+//    public void setIsOldFileVersion(boolean isOldFileVersion) {
+//        this.isOldFileVersion = isOldFileVersion;
+//    }
 }
