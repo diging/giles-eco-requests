1c0cfe47652b4c744aeedeb7e7792619b82d416e
diya17 <diyanna.3@gmail.com>
2023-06-01 16:17:25 -0400

    [GECO-139] Addressing review comments

--- giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/IStorageDeletionRequest.java
+++ giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/IStorageDeletionRequest.java
@@ -1,11 +1,4 @@
 package edu.asu.diging.gilesecosystem.requests;
 
 public interface IStorageDeletionRequest extends IRequest {
-//    public abstract String getStorageFileId();
-//
-//    public abstract void setStorageFileId(String storageFileId);
-//    
-//    public abstract boolean getIsOldFileVersion();
-//
-//    public abstract void setIsOldFileVersion(boolean isOldFileVersion);
 }
--- giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/impl/StorageDeletionRequest.java
+++ giles-eco-requests/src/main/java/edu/asu/diging/gilesecosystem/requests/impl/StorageDeletionRequest.java
@@ -1,41 +1,12 @@
 package edu.asu.diging.gilesecosystem.requests.impl;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import edu.asu.diging.gilesecosystem.requests.FileType;
 import edu.asu.diging.gilesecosystem.requests.IStorageDeletionRequest;
 
 public class StorageDeletionRequest extends Request implements IStorageDeletionRequest  {
     public final static String REQUEST_TYPE = "giles.request_type.storage_deletion";
-    
-//    @JsonProperty
-//    private String storageFileId;
-//    
-//    @JsonProperty
-//    private boolean isOldFileVersion;
-//    
+
     @Override
     public String getType() {
         return REQUEST_TYPE;
     }
-//    
-//    @Override
-//    public String getStorageFileId() {
-//        return storageFileId;
-//    }
-//
-//    @Override
-//    public void setStorageFileId(String storageFileId) {
-//        this.storageFileId = storageFileId;
-//    }
-//
-//    @Override
-//    public boolean getIsOldFileVersion() {
-//        return this.isOldFileVersion;
-//    }
-//
-//    @Override
-//    public void setIsOldFileVersion(boolean isOldFileVersion) {
-//        this.isOldFileVersion = isOldFileVersion;
-//    }
 }
